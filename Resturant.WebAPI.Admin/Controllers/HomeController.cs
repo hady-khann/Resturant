9fcbf160c7bb08a169b0ba971abb80fdc6455560
mohammad hady - khan mohammad pour <41488238+hady-khann@users.noreply.github.com>
2021-11-27 15:34:36 +0330

    Two New Layers Added For Resturants & Admin .

---
+++ Resturant.WebAPI.Admin/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resturant.WebAPI.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+
+        // POST: HomeController/Create
+        
+        [HttpPost]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}

3cef47ce6b12477e16e01e74f3637187b2bd0ccd
mohammad hady - khan mohammad pour <41488238+hady-khann@users.noreply.github.com>
2021-12-01 15:59:07 +0330

    Roles ENUM

---
+++ Resturant.Utilities/Enums/List_Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Utilities.Enums
+{
+    public enum List_Roles
+    {
+        Root,
+        Owner,
+        Manager,
+        Admin,
+        Resturant,
+        Guest,
+    }
+}

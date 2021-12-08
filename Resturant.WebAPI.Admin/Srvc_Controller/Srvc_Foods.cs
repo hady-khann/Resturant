084a420e549acfa623f7df1081c9e90664eb3642
mohammad hady - khan mohammad pour <41488238+hady-khann@users.noreply.github.com>
2021-12-08 08:29:17 +0330

    Only adding Admin Controllers Services Classes

---
+++ Resturant.WebAPI.Admin/Srvc_Controller/Srvc_Foods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Resturant.WebAPI.Admin.Srvc_Controller
+{
+    public class Srvc_Foods
+    {
+    }
+}

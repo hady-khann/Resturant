f440fe2a180fb373a4f7bd82c692727011a7134a
mohammad hady - khan mohammad pour <41488238+hady-khann@users.noreply.github.com>
2021-12-12 09:35:32 +0330

    add password to userinfo context

--- Resturant.DBModels/Entities/UsersInfo.cs
+++ Resturant.DBModels/Entities/UsersInfo.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 
 #nullable disable
 
-namespace Resturant.DBModels.Entities
+namespace Resturant.DataAccess
 {
     public partial class UsersInfo
     {
         public Guid Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
+        public string PassWord { get; set; }
         public int Wallet { get; set; }
         public string Address { get; set; }
         public Guid RoleId { get; set; }
